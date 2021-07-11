62d07b7fe783f830632216e64eb46817431ca0da
Ivan <ivan.kashytski.mkx@gmail.com>
2021-07-11 23:49:50 +0300

    load_on_git

---
+++ Assets/Scripts/P_2.1/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 25;
+    private float lowerBound = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //destroy pizzas out of screen
+        if (transform.position.z > topBound)
+            Destroy(gameObject);
+
+        //destroy animals out of screen
+        if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over!");
+        }
+
+    }
+}
---
+++ Assets/Scripts/P_2.1/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalsPrefabs;
+    private float spawnRangeX = 25;
+    private float spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float timeInterval = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //use SpawnRandomAnimal sometimes
+        InvokeRepeating("SpawnRandomAnimal", startDelay, timeInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //get random animal and random place to spawn
+        int animalIndex = Random.Range(0, animalsPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+        Instantiate(animalsPrefabs[animalIndex], spawnPos, animalsPrefabs[animalIndex].transform.rotation);
+    }
+}
